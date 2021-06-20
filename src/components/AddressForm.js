a9297ee67b33d4bbb8f6ac64d489401cf70b1217
geomin76 <geom@vt.edu>
2021-06-20 11:43:53 -0400

    another upodate

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -56,26 +56,26 @@ const AddressForm = ({ checkoutToken, test }) => {
         <form onSubmit={methods.handleSubmit((data) => test({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6}>
-                <input required name="firstName" placeholder="First name" type="text" className="form-control" style={{ backgroundColor: '#1A1A1D' }}/>
+                <input required name="firstName" placeholder="First name" type="text" className="form-control"/>
             </Grid>
             <Grid item xs={12} sm={6}>
-                <input required name="lastName" placeholder="Last name" type="text" className="form-control" style={{ backgroundColor: '#1A1A1D' }}/>
+                <input required name="lastName" placeholder="Last name" type="text" className="form-control"/>
             </Grid>
             <Grid item xs={12} sm={6}>
-                <input required name="address1" placeholder="Address line 1" type="text" className="form-control" style={{ backgroundColor: '#1A1A1D' }}/>
+                <input required name="address1" placeholder="Address line 1" type="text" className="form-control"/>
             </Grid>
             <Grid item xs={12} sm={6}>
-                <input required name="email" placeholder="Email" type="text" className="form-control" style={{ backgroundColor: '#1A1A1D' }}/>
+                <input required name="email" placeholder="Email" type="text" className="form-control"/>
             </Grid>
             <Grid item xs={12} sm={6}>
-                <input required name="city" placeholder="City" type="text" className="form-control" style={{ backgroundColor: '#1A1A1D' }}/>
+                <input required name="city" placeholder="City" type="text" className="form-control"/>
             </Grid>
             <Grid item xs={12} sm={6}>
-                <input required name="zip" placeholder="Zip / Postal Code" type="text" className="form-control" style={{ backgroundColor: '#1A1A1D' }}/>
+                <input required name="zip" placeholder="Zip / Postal Code" type="text" className="form-control"/>
             </Grid>
             
-            <Grid item xs={12} sm={6} >
-              <InputLabel style={{ color: "#FFFFFF" }}>Shipping Country</InputLabel>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
               <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
                 {Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name })).map((item) => (
                   <MenuItem key={item.id} value={item.id}>
@@ -85,7 +85,7 @@ const AddressForm = ({ checkoutToken, test }) => {
               </Select>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <InputLabel style={{ color: "#FFFFFF" }}>Shipping Subdivision</InputLabel>
+              <InputLabel>Shipping Subdivision</InputLabel>
               <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
                 {Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name })).map((item) => (
                   <MenuItem key={item.id} value={item.id}>
@@ -94,7 +94,7 @@ const AddressForm = ({ checkoutToken, test }) => {
                 ))}
               </Select>
             </Grid>
-            {/* <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Options</InputLabel>
               <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
                 {shippingOptions.map((sO) => ({ id: sO.id, label: `${sO.description} - (${sO.price.formatted_with_symbol})` })).map((item) => (
@@ -103,11 +103,11 @@ const AddressForm = ({ checkoutToken, test }) => {
                   </MenuItem>
                 ))}
               </Select>
-            </Grid> */}
+            </Grid>
           </Grid>
           <br />
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Button component={Link} variant="outlined" color="primary" to="/cart">Back to Cart</Button>
+            <Button component={Link} variant="outlined" to="/cart">Back to Cart</Button>
             <Button type="submit" variant="contained" color="primary">Next</Button>
           </div>
         </form>
