29f4debb2b7ae604bf085a012b722c8839d96aa1
geomin76 <geom@vt.edu>
2021-06-22 10:23:05 -0400

    Adding cart item delet

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,16 +1,16 @@
 import React from "react"
-import { MDBBox, MDBCol, MDBContainer, MDBJumbotron, MDBRow, MDBNavLink, MDBListGroup, MDBListGroupItem } from "mdbreact";
+import { MDBBox, MDBCol, MDBContainer, MDBJumbotron, MDBRow, MDBNavLink, MDBListGroup, MDBListGroupItem, MDBBtn } from "mdbreact";
 import { Navbar } from "../components/Navbar";
 import Increment from "../components/Increment";
 
 export const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart }) => {
 
-    console.log(cart)
     return (
         <MDBContainer>
             <Navbar cart={cart}/>
             <MDBRow>
                 <MDBCol md="8">
+                    <h3>Items</h3>
                     <MDBListGroup>
                     {
                         cart.line_items && cart.line_items.map((item, index) => {
@@ -18,9 +18,10 @@ export const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart }) => {
                                 <MDBListGroupItem style={{ backgroundColor: "#1A1A1D" }}>
                                     <div style={{ justifyContent: "space-between"}}>
                                         <img style={{ display: "inline-block", padding: "5px" }} src={item.media.source} width="40%" alt="img"/>
-                                        <h4 style={{ display: "inline-block", padding: "5px" }}>{item.product_name}</h4>
+                                        <p style={{ display: "inline-block", padding: "5px", fontSize: "20px" }}>{item.product_name}</p>
                                         <div style={{ display: "inline-block", padding: "5px" }}><Increment qty={item.quantity} onUpdateCartQty={onUpdateCartQty} id={item.id}/></div>
                                         <p style={{ display: "inline-block", padding: "5px" }}>{item.line_total.formatted_with_symbol}</p>
+                                        <p style={{ display: "inline-block", padding: "5px" }} onClick={() => onRemoveFromCart(item.id)}>Remove</p>
                                     </div>                                    
                                 </MDBListGroupItem>
                             )  
@@ -31,7 +32,7 @@ export const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart }) => {
 
                 <MDBCol md="4">
                     <p>Total: {cart.subtotal && cart.subtotal.formatted_with_symbol}</p>
-                    <MDBNavLink to="/checkout">Checkout</MDBNavLink>
+                    <MDBNavLink to="/checkout"><MDBBtn>Checkout</MDBBtn></MDBNavLink>
                 </MDBCol>
             </MDBRow>
         </MDBContainer>
