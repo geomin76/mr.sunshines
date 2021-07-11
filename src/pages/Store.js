eb1fbad699b8e22d151ed918f2ee28a8c7d8ece8
geomin76 <geom@vt.edu>
2021-07-11 16:43:45 -0400

    Adding 404 page

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -4,7 +4,6 @@ import { Navbar } from "../components/Navbar";
 import { useHistory } from "react-router-dom";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 export const Store = ({ products, addToCart, cart }) => {
 
